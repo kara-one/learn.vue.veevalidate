9a4eea95f48d1333c1e2c2ce4417ab8d0d82e2ce
Serhii Karavashkin <ksawl@narod.ru>
2020-08-30 16:48:35 +0300

    cf: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    amd: true,
+    commonjs: true,
+  },
+  globals: {
+    process: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    'ecmaVersion': 2020,
+    'sourceType': 'module',
+    'parser': 'babel-eslint'
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'eqeqeq': 'off',
+    'curly': 'error',
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error', 'never'
+    ],
+    'indent': ['error', 2]
+  },
+}
